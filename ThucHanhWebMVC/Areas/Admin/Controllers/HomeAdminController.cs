b83825a0400fd5615f84d06d28177ca1e54f43e5
huytt268 <tuanhuytruong35@gmail.com>
2024-10-25 08:24:44 +0700

    feat: add layout for admin page

---
+++ ThucHanhWebMVC/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThucHanhWebMVC.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin")]
+    [Route("admin/homeadmin")]
+    public class HomeAdminController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
