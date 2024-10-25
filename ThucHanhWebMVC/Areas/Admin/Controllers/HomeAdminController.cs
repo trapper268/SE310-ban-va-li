48aa0e3ac85d4836c447e8b440872f07ca23b1d4
huytt268 <tuanhuytruong35@gmail.com>
2024-10-25 09:00:16 +0700

    feat: add product category page for admin

--- ThucHanhWebMVC/Areas/Admin/Controllers/HomeAdminController.cs
+++ ThucHanhWebMVC/Areas/Admin/Controllers/HomeAdminController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ThucHanhWebMVC.Models;
+using X.PagedList;
 
 namespace ThucHanhWebMVC.Areas.Admin.Controllers
 {
@@ -7,11 +12,25 @@ namespace ThucHanhWebMVC.Areas.Admin.Controllers
     [Route("admin/homeadmin")]
     public class HomeAdminController : Controller
     {
+        QlbanVaLiContext db = new QlbanVaLiContext();
+
+
         [Route("")]
         [Route("index")]
         public IActionResult Index()
         {
+            
             return View();
+        }
+
+        [Route("danhmucsanpham")]
+        public IActionResult DanhMucSanPham(int? page)
+        {
+            int pageSize = 12;
+            int pageNumber = (int)((page == null || page < 0) ? 1 : page);
+            var listsanpham = db.TDanhMucSps.AsNoTracking().OrderBy(x => x.TenSp);
+            PagedList<TDanhMucSp> lst = new PagedList<TDanhMucSp>(listsanpham, pageNumber, pageSize);
+            return View(lst);
         }
     }
 }
